be6253127cb8cd1ae95128af5f024a29f0d96ff8
Feng Yue <fyue@yinxiang.com>
2019-08-04 04:08:43 +0000

    increase mem limit for api module

--- bin/commands/docker/countly-api.sh
+++ bin/commands/docker/countly-api.sh
@@ -12,4 +12,4 @@ then
 	echo "[api] MongoDB started"
 fi
 
-exec /sbin/setuser countly /usr/bin/nodejs --max-old-space-size=4096 /opt/countly/api/api.js
+exec /sbin/setuser countly /usr/bin/nodejs --max-old-space-size=8192 /opt/countly/api/api.js
